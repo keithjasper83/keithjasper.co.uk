6236d747b1f3403e0f4f65dc982b6da3d1c4abdc
Keith Jasper <keith_jasper83@icloud.com>
2024-05-05 16:12:50 +0100

    sadsd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Nav } from "./components/layout/nav";
-import { Footer } from "./components/layout/footer";
-import { ContentWrapper } from "./components/layout/content";
+import { Nav } from "../components/layout/nav";
+import { Footer } from "../components/layout/footer";
+import { ContentWrapper } from "../components/layout/content";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
