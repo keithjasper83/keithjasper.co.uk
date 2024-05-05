045f858b1f08785266cc16dbeecd471e49c92e25
Keith Jasper <keith_jasper83@icloud.com>
2024-05-05 16:43:44 +0100

    dfg

--- app/apitest/page.tsx
+++ app/apitest/page.tsx
@@ -1,54 +1,26 @@
-// /app/apitest/page.tsx
+//import { Head } from "next/document";
+import { Metadata } from "next";
 
-import { GetServerSideProps } from "next";
 import Head from "next/head";
-import getConfig from "next/config";
 
-interface User {
-  id: number;
-  name: string;
-  // Add more properties as needed
-}
+export const metadata: Metadata = {
+  title: "Contact",
+};
 
-const { serverRuntimeConfig } = getConfig();
-const API_URL = serverRuntimeConfig.API_URL;
-
-const ApiTest = ({ users }: { users: User[] }) => {
+const ApiTest = async () => {
+  const dynamicData = await fetch(`http://localhost:5129/Users`, {
+    cache: "no-store",
+  });
+  console.log(dynamicData);
   return (
     <>
       <Head>
         <title>ApiTest</title>
       </Head>
       <h1>ApiTest</h1>
-      <div>
-        {/* Display fetched users */}
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      </div>
+      <div></div>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    return {
-      props: {
-        users: data,
-      },
-    };
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    return {
-      props: {
-        users: [],
-      },
-    };
-  }
 };
 
 export default ApiTest;
