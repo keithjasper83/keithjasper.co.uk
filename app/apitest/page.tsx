0c763c49168df697bfae4a8a70b283742b8fac62
Keith Jasper <keith_jasper83@icloud.com>
2024-05-05 15:37:57 +0100

    sdfsdf

---
+++ app/apitest/page.tsx
@@ -0,0 +1,32 @@
+import { use } from "react";
+import Head from "next/head";
+
+async function getNames() {
+  return await await fetch("http://localhost:5129/Users", {
+    cache: "no-store",
+  });
+}
+
+const ApiTest = () => {
+  // State to store the fetched users
+  const users = use(getNames());
+
+  return (
+    <>
+      <Head>
+        <title>ApiTest</title>
+      </Head>
+      <h1>ApiTest</h1>
+      <div>
+        {/* Display fetched users */}
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default ApiTest;
