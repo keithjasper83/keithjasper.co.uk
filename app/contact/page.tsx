e77d12f6796f4f15799687bb9d3f00cf494ef0c6
brunar <brunaradeke@hotmail.com>
2024-05-06 21:16:02 +0100

    css

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,17 @@
-//import { Head } from "next/document";
-import { Metadata } from 'next';
-
 import Head from 'next/head';
+import { Metadata } from 'next';
+import Link from 'next/link';
+import { Icons } from '../../components/icons';
+
+const skills = [
+  {
+    avatar: 'Push to deploy',
+    title: 'Push to deploy',
+    content:
+      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+    icon: 'linkedin',
+  },
+];
 
 export const metadata: Metadata = {
   title: 'Contact',
@@ -12,9 +22,197 @@ const Contact = () => {
       <Head>
         <title>Contact</title>
       </Head>
+
+      <div>
+        <div>
+          <img
+            className="h-32 w-full object-cover lg:h-48"
+            src="https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            alt=""
+          />
+        </div>
+        <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+          <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+            <div className="flex">
+              <img
+                className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+                src="https://media.licdn.com/dms/image/D4D03AQHTXFHmY1bkeg/profile-displayphoto-shrink_200_200/0/1691477435676"
+                alt=""
+              />
+            </div>
+            <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+              <div className="mt-6 min-w-0 flex-1 sm:hidden md:block">
+                <h1 className="truncate text-2xl font-bold text-gray-200">
+                  Keith Jasper
+                </h1>
+              </div>
+              <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-x-4 sm:space-y-0">
+                <Link href="mailto:keith@keithjasper.co.uk" passHref>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                  >
+                    <Icons name="linkedin" aria-hidden="true" />
+                    Email
+                  </button>
+                </Link>
+                <Link href="tel:+4482128679" passHref>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                  >
+                    <Icons name="linkedin" aria-hidden="true" />
+                    Call me
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 hidden min-w-0 flex-1 sm:block md:hidden">
+            <h1 className="truncate text-2xl font-bold text-gray-900">
+              Keith Jasper
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="row"></div>
+
       <div className="row">
-        <h1>Contact</h1>
-        <p>Use social links below to contact me.</p>
+        <div>
+          <h1>Contact</h1>
+
+          <div className="flex gap-x-4">
+            <dt className="flex-none">
+              <span className="sr-only">Email</span>
+              <Icons name="envelope-o" aria-hidden="true" />
+            </dt>
+            <dd>
+              <Link
+                className="hover:text-orange-500"
+                href="mailto:keith@keithjasper.co.uk"
+              >
+                keith@keithjasper.co.uk
+              </Link>
+            </dd>
+          </div>
+          <div className="flex gap-x-4">
+            <dt className="flex-none">
+              <span className="sr-only">Message me</span>
+              <Icons name="envelope-o" aria-hidden="true" />
+            </dt>
+            <dd>
+              <Link
+                className="hover:text-orange-500"
+                href="https://www.linkedin.com/in/keith-jasper-uk/"
+              >
+                LinkedIn
+              </Link>
+            </dd>
+          </div>
+        </div>
+        <form
+          action="#"
+          method="POST"
+          className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
+        >
+          <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-semibold leading-6 text-white"
+                >
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm font-semibold leading-6 text-white"
+                >
+                  Last name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-semibold leading-6 text-white"
+                >
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="phone-number"
+                  className="block text-sm font-semibold leading-6 text-white"
+                >
+                  Phone number
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="tel"
+                    name="phone-number"
+                    id="phone-number"
+                    autoComplete="tel"
+                    className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-semibold leading-6 text-white"
+                >
+                  Message
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows={4}
+                    className="block w-full rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                    defaultValue={''}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="mt-8 flex justify-end">
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Send message
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </>
   );
--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from 'next';
-
+import { Icons } from '../../components/icons';
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -14,76 +14,137 @@ const Games = () => {
         <title>Games</title>
       </Head>
       <div className="row">
-        <h1 className="py-10">Games</h1>
-        <h2>KJ Scorched Earth</h2>
-        <div className="flex">
-          <Image
-            src={'/assets/images/KJScorched-Still.png'}
-            alt="Scorched Screeenshot"
-            width="320"
-            height="240"
-          />
-          <video
-            width="320"
-            height="240"
-            controls
-            preload="/assets/images/KJScorched-Still.png"
-          >
-            <source src="/assets/video/KJScorched-Video.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+        <div className="py-20">
+          <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+            <div className="mx-auto max-w-2xl lg:mx-0">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl">
+                Games
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-500">
+                Developed on C++ games and bellow has some scrrenshots and video
+                about the game and feel free to install and play it. Test it
+                out!
+              </p>
+            </div>
+            <ul
+              role="list"
+              className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-8 xl:col-span-2"
+            >
+              <li>
+                <Image
+                  className="rounded-2xl"
+                  src={'/assets/images/KJScorched-Still.png'}
+                  alt="Scorched Screeenshot"
+                  width="320"
+                  height="240"
+                />
+                <h3 className="mt-6 text-lg font-semibold leading-8 text-gray-200">
+                  KJ Scorched Earth
+                </h3>
+                <p className="text-base leading-7 text-gray-500">
+                  Developement started on 2023
+                </p>
+                <p className="mt-4 text-base leading-7 text-gray-500">
+                  NOTE: Currently only available for Windows and has only been
+                  tested on Windows 11. Some large amount of work is in progress
+                  to make Cross Platform (Windows, OSX and Linux)
+                </p>
+                <ul role="list" className="mt-6 flex gap-x-6">
+                  <li>
+                    <Link
+                      className="text-gray-400 hover:text-gray-500"
+                      href="/downloads/KJScorched.zip"
+                    >
+                      <Icons name="download" />
+                      Download
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-gray-400 hover:text-gray-500"
+                      href="https://github.com/keithjasper83/Scorched2024"
+                      target="_blank"
+                    >
+                      <Icons name="github" />
+                      View Code
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <video
+                  width="320"
+                  height="240"
+                  controls
+                  preload="/assets/images/KJScorched-Still.png"
+                >
+                  <source
+                    src="/assets/video/KJScorched-Video.mp4"
+                    type="video/mp4"
+                  />
+                  Your browser does not support the video tag.
+                </video>
+              </li>
+
+              <li>
+                <Image
+                  className="rounded-2xl"
+                  src={'/assets/images/DevilApproaches-Still.png'}
+                  alt="Devil Approaches Screenshot"
+                  width="320"
+                  height="240"
+                />
+                <h3 className="mt-6 text-lg font-semibold leading-8 text-gray-200">
+                  Devil Approaches
+                </h3>
+                <p className="text-base leading-7 text-gray-500">
+                  Developement started on 2024
+                </p>
+                <p className="mt-4 text-base leading-7 text-gray-500">
+                  NOTE: Currently only available for Windows and has only been
+                  tested on Windows 11. Some progress has been made to make
+                  Cross Platform (Windows, OSX and Linux) however that is still
+                  ongoing.
+                </p>
+                <ul role="list" className="mt-6 flex gap-x-6">
+                  <li>
+                    <Link
+                      className="text-gray-400 hover:text-gray-500"
+                      href="/downloads/DevilApproaches.zip"
+                    >
+                      <Icons name="download" />
+                      Download Game
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="text-gray-400 hover:text-gray-500"
+                      href="https://github.com/keithjasper83/DevilApproaches"
+                      target="_blank"
+                    >
+                      <Icons name="github" />
+                      View Code
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <video
+                  width="320"
+                  height="240"
+                  controls
+                  preload="/assets/images/DevilApproaches-Still.png"
+                >
+                  <source
+                    src="/assets/video/DevilApproaches-Video.mp4"
+                    type="video/mp4"
+                  />
+                  Your browser does not support the video tag.
+                </video>
+              </li>
+            </ul>
+          </div>
         </div>
-        <p>
-          NOTE: Currently only available for Windows and has only been tested on
-          Windows 11. Some large amount of work is in progress to make Cross
-          Platform (Windows, OSX and Linux)
-        </p>
-        <p>
-          <Link href="/downloads/KJScorched.zip">Download</Link> -{' '}
-          <Link
-            href="https://github.com/keithjasper83/Scorched2024"
-            target="_blank"
-          >
-            View on GitHub.com
-          </Link>
-        </p>
-      </div>
-      <div className="row py-10">
-        <h2>DevilApproaches</h2>
-        <div className="flex">
-          <Image
-            src={'/assets/images/DevilApproaches-Still.png'}
-            alt="Devil Approaches Screenshot"
-            width="320"
-            height="240"
-          />
-          <video
-            width="320"
-            height="240"
-            controls
-            preload="/assets/images/DevilApproaches-Still.png"
-          >
-            <source
-              src="/assets/video/DevilApproaches-Video.mp4"
-              type="video/mp4"
-            />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        <p>
-          NOTE: Currently only available for Windows and has only been tested on
-          Windows 11. Some progress has been made to make Cross Platform
-          (Windows, OSX and Linux) however that is still ongoing.
-        </p>
-        <p>
-          <Link href="/downloads/DevilApproaches.zip">Download</Link> -{' '}
-          <Link
-            href="https://github.com/keithjasper83/DevilApproaches"
-            target="_blank"
-          >
-            View on GitHub.com
-          </Link>
-        </p>
       </div>
     </>
   );
