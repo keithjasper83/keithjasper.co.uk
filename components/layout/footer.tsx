701a98819c1ac127bcd3219ea12ae2984deb498b
Keith Jasper <keith_jasper83@icloud.com>
2024-05-08 20:22:00 +0100

    added counter and api demo links - a 90's style counter, does nothing more clever than increase number every api call

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,14 +1,38 @@
+"use client";
+import { useState, useEffect } from "react";
+
 import Link from "next/link";
 import { Icons } from "../icons";
 
 export const Footer = () => {
+  const [counter, setCounter] = useState(null);
+
+  useEffect(() => {
+    const fetchCounter = async () => {
+      try {
+        const res = await fetch(
+          "https://www.keithjasper.co.uk/realapi/counter/"
+        );
+        if (!res.ok) {
+          throw new Error("Failed to fetch counter");
+        }
+        const data = await res.json();
+        setCounter(data); // assuming the response is a number
+      } catch (error) {
+        console.error("Error fetching counter:", error);
+      }
+    };
+
+    fetchCounter();
+  }, []);
+
   const today = new Date();
   return (
     <footer className=" w-full flex justify-center bg-[linear-gradient(_var(--tw-gradient-stops))] to-[#13191f] via-[#13191f] from-[#13191f] border-t-2 border-[#1e2730] p-1 m-0">
       <div className="row flex justify-around py-3">
         <ul>
           <li>&copy; Keith Jasper {today.getFullYear()}</li>
-          <li></li>
+          <li>Counter: {counter} (90's style visit count)</li>
           <li></li>
         </ul>
         <ul>
@@ -58,6 +82,20 @@ export const Footer = () => {
               <Icons name="github" />
               GitHub
             </Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link href="/apitest/documentation/" className="flex items-center">
+              API Docs
+            </Link>{" "}
+            <i>EXAMPLE</i>
+          </li>
+          <li>
+            <Link href="/apitest/users/" className="flex items-center">
+              Users
+            </Link>{" "}
+            <i>EXAMPLE</i>
           </li>
         </ul>
       </div>
